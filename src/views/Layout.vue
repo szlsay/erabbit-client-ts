7828ae2be5e965ddc890e3d20b3e45891ebff6f4
szlsay <409178030@qq.com>
2021-12-27 16:10:30 +0800

    02-目录调整

---
+++ src/views/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Layout</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+</style>
